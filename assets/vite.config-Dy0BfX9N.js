af4184544ddd80f7190105ca295b3d1368c02a9e
Mistry Janam <mistryjanam1010@gmail.com>
2026-04-12 14:59:21 +0530

    Updates

---
+++ assets/vite.config-Dy0BfX9N.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/Shoes_Ecommerce_Website_Shopease/',
+  plugins: [react()],
+})
