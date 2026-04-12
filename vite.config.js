5aad555a893991d701b0499da6f5624aaa46472f
Mistry Janam <mistryjanam1010@gmail.com>
2026-04-12 15:07:04 +0530

    Initial deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Shoes_Ecommerce_Website_Shopease/',
   plugins: [react()],
 })
