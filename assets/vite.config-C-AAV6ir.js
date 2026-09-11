4cc9d29db59e74541947c779ea4ef55d8653bbb4
JANAM1010 <mistryjanam10@email.com>
2026-09-12 03:08:09 +0530

    Updates

--- assets/vite.config-C-AAV6ir.js
+++ assets/vite.config-C-AAV6ir.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Shoes_Ecommerce_Website_Shopease/',
+  // base: '/Shoes_Ecommerce_Website_Shopease/',
+  base: '/shopease/',
   plugins: [react()],
 })
