d29269da10d17c75df295ce3da2feb67aa528f37
JANAM1010 <mistryjanam10@email.com>
2026-09-12 10:11:53 +0530

    Updates

--- assets/vite.config-wuSH-Ejm.js
+++ assets/vite.config-wuSH-Ejm.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // base: '/Shoes_Ecommerce_Website_Shopease/',
-  base: '/shopease/',
   plugins: [react()],
+  base: '/Shoes_Ecommerce_Website_Shopease/',
 })
